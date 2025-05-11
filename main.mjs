3e034f8060f84fee9b3cb13fd1c06f8ba9188dbc
kanata <ta3wata@gmail.com>
2025-05-11 14:19:37 +0900

    first commit

---
+++ main.mjs
@@ -0,0 +1,131 @@
+import express, { request } from "express";
+import { PrismaClient } from "./generated/prisma/index.js";
+
+const app = new express();
+app.use(express.static("./public"));
+app.use(express.json());
+
+const client = new PrismaClient();
+
+/*
+const users = [
+    {
+        id: 1,
+        x: 300,
+        y: 100
+    },
+    {
+        id: 2,
+        x: 200,
+        y: 100
+    },
+    {
+        id: 3,
+        x: 200,
+        y: 200
+    }
+];
+*/
+
+let requestCount = 0;
+let timeElapsed = 0;
+setInterval(() => {
+    timeElapsed++;
+    // console.log("total:", requestCount)
+    // console.log("req / s:", requestCount / timeElapsed)
+}, 1000)
+class Users {
+    array_users = [];
+    constructor() {
+        this.loadUsers();
+    }
+
+    async loadUsers() {
+        this.array_users = [];
+        const users = await client.position.findMany();
+        for (let i = 0; i < users.length; i++) {
+            this.add({ email: users[i].email, username: users[i].username, x: users[i].x, y: users[i].y });
+        }
+    }
+
+    add(position) {
+        this.array_users.push(position);
+    }
+
+    getArray() {
+        console.log(this.array_users);
+        return this.array_users;
+    }
+}
+
+const users = new Users();
+
+app.post("/sendmessage", async (request, response) => {
+    await client.chatroom.create({ data: { email_speaker: request.body.email_speaker, name_speaker: request.body.name_speaker, email_listener: request.body.email_listener, name_listener: request.body.name_listener, message: request.body.message } });
+});
+
+app.post("/getmessage", async (request, response) => {
+    const messages = await client.chatroom.findMany();
+    const obj_response = [];
+    for (let i = 0; i < messages.length; i++) {
+        if (messages[i].email_speaker === request.body.email_speaker && messages[i].email_listener === request.body.email_listener) {
+            obj_response.push({ email_speaker: messages[i].email_speaker, name_speaker: messages[i].name_speaker, email_listener: messages[i].email_listener, name_listener: messages[i].name_listener, message: messages[i].message });
+        }
+        else if (messages[i].email_speaker === request.body.email_listener && messages[i].email_listener === request.body.email_speaker) {
+            obj_response.push({ email_speaker: messages[i].email_speaker, name_speaker: messages[i].name_speaker, email_listener: messages[i].email_listener, name_listener: messages[i].name_listener, message: messages[i].message });
+        }
+    }
+    response.send(JSON.stringify(obj_response));
+});
+
+app.get("/users", async (request, response) => {
+    await users.loadUsers();
+    response.send(JSON.stringify(users.getArray()));
+});
+
+app.post("/signUp", async (request, response) => {
+    //response.send(`名前：${request.body.username}、Eメール：${request.body.email}`);
+    const user = await client.user.findUnique({
+        where: {
+            email: request.body.email
+        }
+    });
+    if (user === null) {
+        await client.user.create({ data: { email: request.body.email, username: request.body.username, password: request.body.password } });
+        const obj_position = { email: request.body.email, username: request.body.username, x: Math.trunc(Math.random() * 1920), y: Math.trunc(Math.random() * 1080) };
+        await client.position.create({ data: obj_position });
+        users.add(obj_position);
+        const message = { status: "success", email: request.body.email, username: request.body.username };
+        response.json(message);
+    }
+    else {
+        const message = { status: "failed" };
+        response.json(message);
+    }
+});
+
+app.post("/signIn", async (request, response) => {
+    const user = await client.user.findUnique({
+        where: {
+            email: request.body.email
+        }
+    });
+    if (user !== null && request.body.password === user.password) {
+        const message = { status: "success", email: request.body.email, username: request.body.username };
+        response.json(message);
+    }
+    else {
+        const message = { status: "failed", message: "メールアドレスかパスワードが間違っています" };
+        response.json(message);
+    }
+});
+
+app.post("/save", async (request, response) => {
+    requestCount++;
+    console.log(request.body)
+    const result = await client.position.update({ where: { email: request.body.email }, data: { x: request.body.x, y: request.body.y } });
+    console.log(result, "☆", "total:", requestCount);
+    response.send("ok")
+});
+
+app.listen(3000);
---
+++ public/script.js
@@ -0,0 +1,432 @@
+// @ts-check
+class Entity {
+    id;
+    x;
+    y;
+    speed = 2;
+    image;
+    color;
+    email;
+    username;
+
+    constructor(email, username, x, y) {
+        this.color = "red";
+        this.x = x;
+        this.y = y;
+        this.email = email;
+        this.username = username;
+    }
+
+    up() {
+        this.y -= this.speed;
+    }
+    down() {
+        this.y += this.speed;
+    }
+    right() {
+        this.x += this.speed;
+    }
+    left() {
+        this.x -= this.speed;
+    }
+
+    setColor(color) {
+        this.color = color;
+    }
+
+    draw(context) {
+        context.fillStyle = this.color;
+        context.beginPath();
+        context.arc(this.x, this.y, 20, 0, 2 * Math.PI);
+        context.closePath();
+        context.fill();
+    }
+
+    collisionDetect(e) {
+        return Math.sqrt((this.x - e.x) * (this.x - e.x) + (this.y - e.y) * (this.y - e.y)) <= 40;
+    }
+
+    getx(){
+        return this.x;
+    }
+
+    gety(){
+        return this.y;
+    }
+}
+
+class Page{
+    body;
+}
+
+class ChatPage extends Page{
+    myEmail;
+    myName;
+    yourEmail;
+    yourName;
+
+    timer;
+
+    h1;
+    div;
+    input;
+    button_send;
+    button_back;
+
+    constructor(myEmail, myName, yourEmail, yourName){
+        super();
+        this.myEmail = myEmail;
+        this.myName = myName;
+        this.yourEmail = yourEmail;
+        this.yourName = yourName;
+        this.launch();
+    }
+
+    launch(){
+        this.body = document.getElementById("body");
+        this.h1 = document.createElement("h1");
+        this.h1.textContent = this.yourName;
+        this.body.appendChild(this.h1);
+        this.div = document.createElement("div");
+        this.body.appendChild(this.div);
+        this.input = document.createElement("input");
+        this.body.appendChild(this.input);
+        this.button_send = document.createElement("button");
+        this.body.appendChild(this.button_send);
+        this.button_send.textContent = "送信";
+        this.button_back = document.createElement("button");
+        this.body.appendChild(this.button_back);
+        this.button_back.textContent = "戻る";
+
+        this.button_send.onclick = async() => {
+            if(this.input.value !== ""){
+                const text = this.input.value;
+                this.input.value = "";
+                const json = JSON.stringify({email_speaker: this.myEmail, name_speaker: this.myName, email_listener: this.yourEmail, name_listener: this.yourName, message: text});
+                await fetch("/sendmessage", {
+                    method: "post",
+                    headers: {"Content-Type": "application/json"},
+                    body: json
+                });
+            }
+        }
+        this.button_back.onclick = () => {
+            this.destroy();
+            new FieldPage(this.myEmail, this.myName);
+        }
+        this.timer = setInterval(async() => {
+            const json = JSON.stringify({email_speaker: this.myEmail, email_listener: this.yourEmail});
+            const response = await fetch("/getmessage", {
+                method: "post",
+                headers: {"Content-Type": "application/json"},
+                body: json
+            });
+            const text = await response.text();
+            const messages = JSON.parse(text);
+            this.div.innerHTML = "";
+            for(let i = 0; i < messages.length; i++){
+                const p = document.createElement("p");
+                if(messages[i].email_speaker === this.myEmail){
+                    p.className = "me";
+                }
+                else{
+                    p.className = "you";
+                }
+                
+                this.div.appendChild(p);
+                p.textContent = messages[i].message;
+            }
+        }, 1000);
+    }
+
+    destroy(){
+        clearInterval(this.timer);
+        this.body.innerHTML = "";
+    }
+
+}
+
+class FieldPage extends Page{
+    canvas;
+    context;
+    player;
+    npcs;
+    email_player;
+    username_player;
+
+    eventHandler;
+
+    timer;
+
+    constructor(email, username){
+        super();
+        this.email_player = email;
+        this.username_player = username;
+        this.launch();
+    }
+
+    async getUsers() {
+        const response = await fetch("/users");
+        const json = await response.text();
+        console.log(json);
+        const array = JSON.parse(json);
+        return array;
+    }
+
+    async setUsers(){
+        const array = await this.getUsers();
+        this.npcs = [];
+        for (let i = 0; i < array.length; i++) {
+            if(array[i].email === this.email_player){
+                this.player = new Entity(array[i].email, array[i].username, array[i].x, array[i].y);
+            } else {
+                this.npcs.push(new Entity(array[i].email, array[i].username, array[i].x, array[i].y));
+            }
+        }
+        this.drawEntities();
+    }
+
+    async setUsersExceptMyself(){
+        const array = await this.getUsers();
+        console.log(this.npcs);
+        this.npcs = [];
+        for (let i = 0; i < array.length; i++) {
+            if(array[i].email !== this.email_player){
+                this.npcs.push(new Entity(array[i].email, array[i].username, array[i].x, array[i].y));
+            }
+        }
+        console.log(array);
+    }
+
+    async saveCurrentPositionOfPlayer(){
+        await fetch("/save", {
+            method: "post",
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify({email: this.email_player, username: this.username_player, x: this.player.getx(), y: this.player.gety()})
+        });
+    }
+
+    drawEntities(){
+        this.context.clearRect(0,0, 1920, 1080);
+
+        this.player.draw(this.context);
+        
+        for (let i = 0; i < this.npcs.length; i++) {
+            if (this.npcs[i].collisionDetect(this.player)) {
+                this.npcs[i].setColor("blue");
+            }
+            else {
+                this.npcs[i].setColor("red");
+            }
+            this.npcs[i].draw(this.context);
+        }
+    }
+
+    async tryChatting(){
+        for(let i = 0; i < this.npcs.length; i++){
+            if(this.npcs[i].collisionDetect(this.player)){
+                this.destroy();
+                console.log(this.player.username);
+                new ChatPage(this.player.email, this.player.username, this.npcs[i].email, this.npcs[i].username);
+                await this.saveCurrentPositionOfPlayer();
+                break;
+            }
+        }
+    }
+
+    launch = async () => {
+        this.body = document.getElementById("body");
+        this.canvas = document.createElement("canvas");
+        this.canvas.width = 1920;
+        this.canvas.height = 1080;
+        if(this.body !== null){
+            this.body.appendChild(this.canvas);
+        }
+        const button = document.createElement("button");
+        button.onclick = async() => await this.saveCurrentPositionOfPlayer();
+        this.body.appendChild(button);
+        button.textContent = "場所を保存";
+        this.context = this.canvas.getContext("2d");
+        await this.setUsers();
+        this.eventHandler = async (e) => {
+            if(this.context !== null){
+                this.context.clearRect(0, 0, this.canvas.clientWidth, this.canvas.height);
+            }
+            switch (e.key) {
+                case "w":
+                    this.player.up();
+                    break;
+                case "a":
+                    this.player.left();
+                    break;
+                case "s":
+                    this.player.down();
+                    break;
+                case "d":
+                    this.player.right();
+                    break;
+                case "Enter":
+                    await this.tryChatting();
+                    break;
+            }
+            this.player.draw(this.context);
+        
+            this.drawEntities();
+        };
+        document.addEventListener("keydown", this.eventHandler);
+
+        this.timer = setInterval(() => {
+            this.saveCurrentPositionOfPlayer();
+            this.setUsersExceptMyself();
+            this.drawEntities();
+        }, 1000);
+        
+    }
+
+    async destroy() {
+        clearInterval(this.timer);
+        this.body.innerHTML = "";
+        document.removeEventListener("keydown", this.eventHandler);
+    }
+}
+
+class SignUpPage extends Page{
+
+    input_email;
+    input_username;
+    input_password;
+    button;
+    signInButton;
+
+    str_email;
+    str_username;
+    str_password;
+
+    constructor(){
+        super();
+        this.launch();
+    }
+
+    launch(){
+        this.body = document.getElementById("body");
+        this.body.innerHTML=`
+            <h4>サインアップ</h4>
+            <div>
+                <p>メールアドレス</p>
+                <input id="email" />
+            </div>
+            <div>
+                <p>ユーザーネーム</p>
+                <input id="username" />
+            </div>
+            <div>
+                <p>パスワード</p>
+                <input id="password" type="password"/>
+            </div>
+            <button id="button">送信</button>
+            <button id="sign_in">サインインページへ</button>
+        `
+        this.input_email = document.getElementById("email");
+        this.input_username = document.getElementById("username");
+        this.input_password = document.getElementById("password");
+        this.button = document.getElementById("button");
+        this.signInButton = document.getElementById("sign_in");
+        this.button.onclick = async () => {
+            if(this.input_email.value !== "" && this.input_username.value !== "" && this.input_password.value !== ""){
+                const json_request = JSON.stringify({email: this.input_email.value, username: this.input_username.value, password: this.input_password.value});
+                const response = await fetch("/signUp", {
+                    method: "post",
+                    headers: {"Content-Type": "application/json" },
+                    body: json_request
+                });
+                const text = await response.text();
+                const json_response = JSON.parse(text);
+                if(json_response.status === "success"){
+                    alert("成功しました");
+                    this.destroy();
+                    new FieldPage(json_response.email, json_response.username);
+                }
+                else{
+                    alert("失敗しました");
+                }
+            }
+        }
+        this.signInButton.onclick = () => {
+            this.destroy();
+            new SignInPage();
+        }
+    }
+
+    destroy(){
+        this.body.innerHTML="";
+    }
+    
+}
+
+class SignInPage extends Page{
+
+    input_email;
+    input_password;
+    button;
+    signUpButton;
+
+    constructor(){
+        super();
+        this.launch();
+    }
+
+    launch(){
+        this.body = document.getElementById("body");
+        this.body.innerHTML=`
+            <h4>サインイン</h4>
+            <div>
+                <p>メールアドレス</p>
+                <input id="email" />
+            </div>
+            <div>
+                <p>パスワード</p>
+                <input id="password" type="password"/>
+            </div>
+            <button id="button">送信</button>
+            <button id="sign_up">サインアップページへ</button>
+        `
+
+        this.input_email = document.getElementById("email");
+        this.input_password = document.getElementById("password");
+        this.button = document.getElementById("button");
+        this.signUpButton = document.getElementById("sign_up");
+        this.button.onclick = async () => {
+            if(this.input_email.value !== "" && this.input_password.value !== ""){
+                const json_request = JSON.stringify({email: this.input_email.value, password: this.input_password.value});
+                const response = await fetch("/signIn", {
+                    method: "post",
+                    headers: {"Content-Type": "application/json" },
+                    body: json_request
+                });
+                const text = await response.text();
+                const json_response = JSON.parse(text);
+                if(json_response.status === "success"){
+                    alert("成功しました");
+                    this.destroy();
+                    new FieldPage(json_response.email, json_response.username);
+                }
+                else{
+                    alert(json_response.message);
+                }
+            }
+        }
+        this.signUpButton.onclick = () => {
+            this.destroy();
+            new SignUpPage();
+        }
+    }
+
+    destroy(){
+        this.body.innerHTML="";
+    }
+    
+}
+
+//const fieldPage = new FieldPage();
+const signUpPage = new SignUpPage();
+
